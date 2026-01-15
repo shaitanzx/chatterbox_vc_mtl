1161ec355fe3b07b8c85747a1f0816ea13582158
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-01-15 23:50:48 +0400

    fix

--- app.py
+++ app.py
@@ -1124,7 +1124,7 @@ def create_gradio_interface():
                 vc_convert_btn.click(lambda: (gr.update(interactive=False)),outputs=[vc_convert_btn]) \
                     .then(
                     fn=_vc_wrapper,
-                    inputs=[vc_input_audio, vc_target_audio, disable_watermark_checkbox, vc_pitch_shift,voice_mode_radio_vc,predefined_voice_select_vc,reference_file_select_vc],
+                    inputs=[vc_input_audio, disable_watermark_checkbox, vc_pitch_shift,voice_mode_radio_vc,predefined_voice_select_vc,reference_file_select_vc],
                     outputs=[vc_output_files, vc_output_audio]) \
                     (lambda: (gr.update(interactive=True),gr.update(visible=True)),outputs=[vc_convert_btn,vc_output_audio])
 
